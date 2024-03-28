0cf1b1d6fecaea64eee90a7a6f338499f83cf1f0
Matteo Sala <matteo111997@gmail.com>
2024-03-28 18:31:46 +0100

    feat: handled backend validation

--- resources/views/pages/dashboard/posts/create.blade.php
+++ resources/views/pages/dashboard/posts/create.blade.php
@@ -11,16 +11,27 @@
             <label for="title" class="form-label">Title</label>
             <input
                 type="text"
-                class="form-control"
+                class="form-control @error('title') is-invalid @enderror"
                 name="title"
-                id="title"/>
+                id="title"
+
+                />
         </div>
+       @error('title')
+            <div class="alert alert-danger">
+                {{ $message }}
+            </div>
+        @enderror
         <div class="mb-3">
             <label for="content" class="form-label">Content</label>
-            <textarea class="form-control" name="content" id="content" rows="3"></textarea>
+            <textarea class="form-control @error('title') is-invalid @enderror"" name="content" id="content" rows="3"></textarea>
         </div>
-
-        <button type="submit" class="btn btn-outline-info ">Create a new project</button>
+        @error('content')
+            <div class="alert alert-danger">
+                {{ $message }}
+            </div>
+        @enderror
+        <button type="submit" class="btn btn-outline-info">Create a new project</button>
 
     </form>
 
